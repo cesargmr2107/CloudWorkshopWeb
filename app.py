57a220cf1d01c14694f6d449a5bc7ea38db2d7d0
Cesar Marquez <cgmr210799@gmail.com>
2023-12-13 21:54:52 +0100

    MyBucketList: base app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import pyodbc 
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -8,25 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   return render_template('index.html', items=[{"id": "id1", "description": "Description", "completed": true}])
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
 
 if __name__ == '__main__':
    app.run()
