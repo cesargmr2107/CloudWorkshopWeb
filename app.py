3eeeb0a645ad410a44519e23f435b2c5594acfbe
Cesar Marquez <cgmr210799@gmail.com>
2023-12-15 17:45:19 +0100

    Database connection via AAD managed identity

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import os
 import pyodbc
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import (Flask, render_template,
+                   send_from_directory)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template(
-        'index.html',
-        items=[
-            {"id": "id1", "description": "Description1", "completed": True},
-            {"id": "id2", "description": "Description2", "completed": False}
-        ]
-    )
+
+    # Connect to database
+    db_connection_string = os.environ["DB_CONNECTION_STRING"]
+    db_connection = pyodbc.connect(db_connection_string)
+
+    # Execute query
+    cursor = db_connection.cursor()
+    query = "SELECT [id], [description], [completed] FROM [dbo].[BUCKET_LIST_ITEMS]"
+    cursor.execute(query)
+    items = [row for row in cursor.fetchall()]
+
+    # Render and return templateñ
+    return render_template('index.html', items=items)
 
 
 @app.route('/favicon.ico')
